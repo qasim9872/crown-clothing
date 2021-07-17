7770e0fc30aecf0c4c6fb3f57fc180a3fe666c33
Muhammad Qasim <qasim9872@gmail.com>
2021-07-17 16:26:34 +0100

    added collection item

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./collection-preview.styles.scss";
+import { CollectionItem } from "../collection-item/collection-item.component";
 
 export const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
       {items.slice(0, 4).map((item) => (
-        <div key={item.id}> {item.name} </div>
+        <CollectionItem key={item.id} {...item} />
       ))}
     </div>
   </div>
