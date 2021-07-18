5a51d5efff2267930ebe44644a1fa8dd2c44dfb3
Muhammad Qasim <qasim9872@gmail.com>
2021-07-18 02:43:38 +0100

    saving data to firestore when user logs in

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { HomePage } from "./pages/home-page/home-page.page";
 import { ShopPage } from "./pages/shop-page/shop-page.page";
 import { SignInAndSignUpPage } from "./pages/sign-in-and-sign-up-page/sign-in-and-sign-up.page";
 import { auth } from "./firebase/firebase.core";
+import { createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor(props) {
@@ -20,16 +21,30 @@ class App extends React.Component {
   }
 
   unsubscribeFromAuth = null;
+  unsubscribeFromUser = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((userSnapShot) => {
+          this.setState({
+            currentUser: {
+              id: userSnapShot.id,
+              ...userSnapShot.data(),
+            },
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
   componentWillUnmount() {
     this.unsubscribeFromAuth && this.unsubscribeFromAuth();
+    this.unsubscribeFromUser && this.unsubscribeFromUser();
   }
 
   render() {
