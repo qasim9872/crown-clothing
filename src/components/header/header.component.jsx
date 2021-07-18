5e7bc1a3a98024b072bb0bfde9a3353b610d8863
Muhammad Qasim <qasim9872@gmail.com>
2021-07-18 01:51:11 +0100

    aadded google sign in and styled google button

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom";
 
 import "./header.styles.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { auth } from "../../firebase/firebase.utils";
 
-export const Header = () => {
+export const Header = ({ currentUser }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -19,6 +20,16 @@ export const Header = () => {
         <Link className="option" to="/contact">
           CONTACT
         </Link>
+
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT{" "}
+          </div>
+        ) : (
+          <Link className="option" to="/signIn">
+            SIGN IN
+          </Link>
+        )}
       </div>
     </div>
   );
